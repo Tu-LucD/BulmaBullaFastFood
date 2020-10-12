a88f38c18794a356518f0f19ce8fa44029c6bd7e
James-Gabriel Cortez-Gregoire <1836197@edu.vaniercollege.qc.ca>
2020-10-11 20:34:36 -0400

    SendEmail Method

--- BulmaBullaFastFood/Controllers/HomeController.cs
+++ BulmaBullaFastFood/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using BulmaBullaFastFood.Models;
@@ -50,8 +52,40 @@ namespace BulmaBullaFastFood.Controllers
 
             db.Accounts.Add(accountToCreate);
             db.SaveChanges();
+            SendEmail(accountToCreate);
 
             return RedirectToAction("Index");
+        }
+
+        // Private method to send a confirmation email to the new account's email.
+        private void SendEmail(Account account)
+        {
+            try
+            {
+                // Create a message object using MailMessage
+                MailMessage message = new MailMessage("technotransact@gmail.com", 
+                                                        account.email, 
+                                                        "CONFIRMATION",
+                                                        @"Hello, you are receiving this email because we have confirmed your email into our database! " +
+                                                        "" +
+                                                        "Thank you!");
+                // Allow specify whether or not we can attach/send html in the body of the mail
+                message.IsBodyHtml = true;
+
+                // Specify the smtpClient which handles all the email sending for us
+                // Each mailServer as its own smtp server and port definition
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                // Gmail requires client have enabled SSL
+                client.EnableSsl = true;
+                // Set client credentials used to authenticate the sender("email", "password")
+                client.Credentials = new System.Net.NetworkCredential("technotransact@gmail.com", "techno98741");
+                // Effectively send message
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
