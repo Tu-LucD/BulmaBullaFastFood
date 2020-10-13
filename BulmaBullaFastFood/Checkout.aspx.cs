b8b819dd84a9fd55a4d343d43a9abda3e89802cb
Tu-Luc Duong <1835233@edu.vaniercollege.qc.ca>
2020-10-12 23:04:24 -0400

    adding checkout page

--- BulmaBullaFastFood/Checkout.aspx.cs
+++ BulmaBullaFastFood/Checkout.aspx.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BulmaBullaFastFood
 {
     public partial class Checkout : System.Web.UI.Page
     {
+        String user, food;
         protected void Page_Load(object sender, EventArgs e)
         {
+            String constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|BBFastFoodDB.mdf;Integrated Security=False";
 
+            food = Request.QueryString["ka"].ToString();
+            
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into orders values (2,'" + food + "', 'drink', 'montreal', 'Male', 12.00)";
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
