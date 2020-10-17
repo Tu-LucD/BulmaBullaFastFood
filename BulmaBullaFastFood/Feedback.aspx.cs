76f69e3cc56b9745d17649347fb0621bb25af072
James-Gabriel Cortez-Gregoire <1836197@edu.vaniercollege.qc.ca>
2020-10-17 16:56:54 -0400

    did contact feedback and stuff (just need to actually put info in the newsletter)

---
+++ BulmaBullaFastFood/Feedback.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BulmaBullaFastFood
+{
+    public partial class Feeback : System.Web.UI.Page
+    {
+        public string email, message;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|BBFastFoodDB.mdf;Integrated Security=False";
+
+            email = Request.QueryString["em"].ToString();
+            message = Request.QueryString["mg"].ToString();
+
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into feedback values ('"+email+"', '"+message+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
